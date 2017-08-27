f6ba5cd7b79cc8eebfb07e742c1eeea4166275c4
diptadas <dipta670@gmail.com>
2017-08-28 00:16:00 +0600

    Update: text-template

--- text-template.go
+++ text-template.go
@@ -1,46 +1,52 @@
 package main
 
 import (
-	"log"
 	"os"
 	"text/template"
+	"fmt"
 )
 
-func main() {
-	// Define a template.
-	const letter = `
-Dear {{.Name}},
-{{if .Attended}}
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func example_1() {
+	// define a template
+	const letter = `Dear {{.Name}},
+{{- if .Attended}}
 It was a pleasure to see you at the wedding.
 {{- else}}
 It is a shame you couldn't make it to the wedding.
 {{- end}}
-{{with .Gift -}}
+{{- with .Gift}}
 Thank you for the lovely {{.}}.
-{{end}}
+{{- end}}
 Best wishes,
 Josie
 `
-
-	// Prepare some data to insert into the template.
+	// Prepare some data to insert into the template
 	type Recipient struct {
 		Name, Gift string
 		Attended   bool
 	}
-	var recipients = []Recipient{
+	recipients := []Recipient{
 		{"Aunt Mildred", "bone china tea set", true},
 		{"Uncle John", "moleskin pants", false},
 		{"Cousin Rodney", "", false},
 	}
 
-	// Create a new template and parse the letter into it.
+	// Create a new template and parse the letter into it
 	t := template.Must(template.New("letter").Parse(letter))
 
-	// Execute the template for each recipient.
+	// Execute the template for each recipient
 	for _, r := range recipients {
-		err := t.Execute(os.Stdout, r)
-		if err != nil {
-			log.Println("executing template:", err)
-		}
+		check(t.Execute(os.Stdout, r))
+		fmt.Println("==============")
 	}
+}
+
+func main() {
+	example_1()
 }
