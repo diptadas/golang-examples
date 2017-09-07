0ccd6dc4f93ffd65c5bfff5064fe05be6f578889
diptadas <dipta@appscode.com>
2017-09-07 10:14:50 +0600

    Update: text-template

--- text_template.go
+++ text_template.go
@@ -38,6 +38,38 @@ func example_1() {
 	check(tmp.Execute(os.Stdout, nil))
 }
 
+func example_2() {
+	arr_1 := []int{11, 22, 33}
+	map_1 := map[int]string{5: "AA", 6: "BB", 7: "CC"}
+	map_2 := map[string]string{"a": "A", "b": "B", "c": "C"}
+
+	tmpStr := "Value: {{index . 0}}\n"
+	tmp := template.Must(template.New("test").Parse(tmpStr))
+	check(tmp.Execute(os.Stdout, arr_1))
+
+	tmpStr = "Value: {{index . 5}}\n"
+	tmp = template.Must(template.New("test").Parse(tmpStr))
+	check(tmp.Execute(os.Stdout, map_1))
+
+	tmpStr = "Value: {{.a}}\n"
+	tmp = template.Must(template.New("test").Parse(tmpStr))
+	check(tmp.Execute(os.Stdout, map_2))
+}
+
+func example_3() {
+	tmpStr := "Iterate: {{range .}} Value = {{.}} {{end}}\n"
+	tmp := template.Must(template.New("test").Parse(tmpStr))
+	check(tmp.Execute(os.Stdout, []int{11, 22, 33}))
+
+	tmpStr = "Iterate: {{range $i, $v := .}} {{$i}}:{{$v}} {{end}}\n"
+	tmp = template.Must(template.New("test").Parse(tmpStr))
+	check(tmp.Execute(os.Stdout, []int{11, 22, 33}))
+
+	tmpStr = "Iterate: {{range .}} Value = {{.}} {{else}}Data is empty{{end}}\n"
+	tmp = template.Must(template.New("test").Parse(tmpStr))
+	check(tmp.Execute(os.Stdout, []int{}))
+}
+
 func main() {
 	example_0()
 }
