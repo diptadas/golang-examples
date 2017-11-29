bbac56c21023e58a1f2ba9b90b01c5ef8212ce1f
diptadas <dipta@appscode.com>
2017-11-29 12:06:37 +0600

    Update kube event

--- k8s/event_record/k8s_event.go
+++ k8s/event_record/k8s_event.go
@@ -3,69 +3,58 @@ package main
 import (
 	"log"
 	"os"
+	"fmt"
+	"time"
 
 	core "k8s.io/api/core/v1"
 	kerr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/record"
-	"time"
+	"k8s.io/client-go/tools/reference"
 )
 
 func main() {
 	kubeClient, err := getKubeClient()
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Fatal(err.Error())
 	}
 
 	resource := &core.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: "example-configmap",
+			Name:      "example-configmap",
+			Namespace: "default",
 		},
 	}
 
 	log.Println("Deleting old resource")
-	err = kubeClient.CoreV1().ConfigMaps("default").Delete(resource.Name, &metav1.DeleteOptions{})
+	err = kubeClient.CoreV1().ConfigMaps(resource.Namespace).Delete(resource.Name, &metav1.DeleteOptions{})
 	if err != nil && !kerr.IsNotFound(err) {
-		log.Fatalf(err.Error())
+		log.Fatal(err.Error())
 	}
 
 	log.Println("Creating new resource")
 	resource, err = kubeClient.CoreV1().ConfigMaps("default").Create(resource)
 	if err != nil {
-		log.Fatalf(err.Error())
+		log.Fatal(err.Error())
 	}
 
-	log.Println("Recording events")
-	_, recorder := NewEventRecorder(kubeClient, "golang-examples")
+	log.Println("Recording events using broadcaster")
+	recorder := newEventRecorder(kubeClient, "golang-examples")
+	recorder.Event(resource, core.EventTypeNormal, "event-test-1", "new event is recorded")
+	time.Sleep(time.Second) // time to complete event
 
-	go func() {
-		for i := 0; i < 5; i++ {
-			recorder.Event(resource, core.EventTypeNormal, "event-test", "new event is recorded")
-			time.Sleep(time.Second)
-		}
-	}()
+	log.Println("Creating events directly")
+	event, err := createEvent(kubeClient, "golang-examples", resource, core.EventTypeNormal, "event-test-new-1", "new event is recorded")
+	if err != nil {
+		log.Fatal(err.Error())
 
-	select {}
-}
-
-func NewEventRecorder(client kubernetes.Interface, component string) (watch.Interface, record.EventRecorder) {
-	// Event Broadcaster
-	broadcaster := record.NewBroadcaster()
-	watcher := broadcaster.StartEventWatcher(
-		func(event *core.Event) {
-			if _, err := client.CoreV1().Events(event.Namespace).Create(event); err != nil {
-				log.Println(err)
-			} else {
-				log.Println("Event recorded:", event.Name)
-			}
-		},
-	)
-	// Event Recorder
-	return watcher, broadcaster.NewRecorder(scheme.Scheme, core.EventSource{Component: component})
+	} else {
+		log.Println("Event recorded:", event.Name)
+	}
 }
 
 func getKubeClient() (kubernetes.Interface, error) {
@@ -75,4 +64,44 @@ func getKubeClient() (kubernetes.Interface, error) {
 		return nil, err
 	}
 	return kubernetes.NewForConfig(config)
+}
+
+func newEventRecorder(client kubernetes.Interface, component string) record.EventRecorder {
+	// Event Broadcaster
+	broadcaster := record.NewBroadcaster()
+	broadcaster.StartEventWatcher(
+		func(event *core.Event) {
+			if _, err := client.CoreV1().Events(event.Namespace).Create(event); err != nil {
+				log.Println(err)
+			} else {
+				log.Println("Event recorded:", event.Name)
+			}
+		},
+	)
+	// Event Recorder
+	return broadcaster.NewRecorder(scheme.Scheme, core.EventSource{Component: component})
+}
+
+func createEvent(client kubernetes.Interface, component string, obj runtime.Object, eventType, reason, message string) (*core.Event, error) {
+	ref, err := reference.GetReference(scheme.Scheme, obj)
+	if err != nil {
+		return nil, err
+	}
+
+	t := metav1.Time{Time: time.Now()}
+
+	return client.CoreV1().Events(ref.Namespace).Create(&core.Event{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%v.%x", ref.Name, t.UnixNano()),
+			Namespace: ref.Namespace,
+		},
+		InvolvedObject: *ref,
+		Reason:         reason,
+		Message:        message,
+		FirstTimestamp: t,
+		LastTimestamp:  t,
+		Count:          1,
+		Type:           eventType,
+		Source:         core.EventSource{Component: component},
+	})
 }
