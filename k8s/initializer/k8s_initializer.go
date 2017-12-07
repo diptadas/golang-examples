c888de2dba662e8d9375851d2e492d65aaeab40e
diptadas <dipta@appscode.com>
2017-12-07 15:41:01 +0600

    Update k8s_initializer

--- k8s/initializer/k8s_initializer.go
+++ k8s/initializer/k8s_initializer.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -21,100 +20,141 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
-var (
-	kubeClient kubernetes.Interface
-)
+const initializerName = "com.my.initializer"
 
 func main() {
-	flag.Parse()
+	kubeClient, err := getKubeClient()
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 
+	initConfig := initializerForResources([]string{"configmaps"})
+	fmt.Println("Creating InitializerConfigurations:", initConfig.Name)
+	if _, err = kubeClient.AdmissionregistrationV1alpha1().InitializerConfigurations().Create(&initConfig); err != nil {
+		log.Fatalf(err.Error())
+	}
+	time.Sleep(3 * time.Second) // time for initializer to be ready
+
+	runController(kubeClient)
+
+	// creating resource
+	configMap := core.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "my-configmap",
+			Namespace: metav1.NamespaceDefault,
+		},
+	}
+	fmt.Println("Creating configmap:", configMap.Name)
+	if _, err = kubeClient.CoreV1().ConfigMaps(configMap.Namespace).Create(&configMap); err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println("Getting configmap:", configMap.Name)
+	obj, err := kubeClient.CoreV1().ConfigMaps(configMap.Namespace).Get(configMap.Name, metav1.GetOptions{})
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	fmt.Println("Configmap data:", obj.Data)
+
+	// cleanup
+	if err = kubeClient.CoreV1().ConfigMaps(configMap.Namespace).Delete(configMap.Name, &metav1.DeleteOptions{}); err != nil {
+		log.Fatalf(err.Error())
+	}
+	if err = kubeClient.AdmissionregistrationV1alpha1().InitializerConfigurations().Delete(initConfig.Name, &metav1.DeleteOptions{}); err != nil {
+		log.Fatalf(err.Error())
+	}
+}
+
+func getKubeClient() (kubernetes.Interface, error) {
 	kubeConfigPath := os.Getenv("HOME") + "/.kube/config"
 	config, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
 	if err != nil {
-		log.Fatalf(err.Error())
+		return nil, err
 	}
-	kubeClient, err = kubernetes.NewForConfig(config)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
-	go RunController()
-
-	initConfig := InitializerForResources([]string{"deployments"})
-	fmt.Println("Creating InitializerConfigurations:", initConfig.Name)
-	_, err = kubeClient.AdmissionregistrationV1alpha1().InitializerConfigurations().Create(&initConfig)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
-	time.Sleep(3 * time.Second)
-
-	deploy := Deployment()
-	fmt.Println("Creating Deployment:", deploy.Name)
-	_, err = kubeClient.AppsV1beta1().Deployments(deploy.Namespace).Create(&deploy)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
-	fmt.Println("Getting deploy:", deploy.Name)
-	obj, err := kubeClient.AppsV1beta1().Deployments(deploy.Namespace).Get(deploy.Name, metav1.GetOptions{})
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
-
-	fmt.Println("Status:", obj.Status.String())
-	select {}
+	return kubernetes.NewForConfig(config)
 }
 
-func RunController() {
+func runController(kubeClient kubernetes.Interface) {
 	lw := &cache.ListWatch{
 		ListFunc: func(options metav1.ListOptions) (runtime.Object, error) {
 			options.IncludeUninitialized = true
-			return kubeClient.AppsV1beta1().Deployments(core.NamespaceAll).List(options)
+			return kubeClient.CoreV1().ConfigMaps(core.NamespaceAll).List(options)
 		},
 		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
 			options.IncludeUninitialized = true
-			return kubeClient.AppsV1beta1().Deployments(core.NamespaceAll).Watch(options)
+			return kubeClient.CoreV1().ConfigMaps(core.NamespaceAll).Watch(options)
 		},
 	}
 
 	resyncPeriod := 30 * time.Second
 
-	_, controller := cache.NewInformer(lw, &apps.Deployment{}, resyncPeriod,
+	_, controller := cache.NewInformer(
+		lw,
+		&core.ConfigMap{},
+		resyncPeriod,
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				deploy := obj.(*apps.Deployment)
-				fmt.Println("Added deployment:", deploy.Name)
-				if deploy.GetInitializers() != nil && len(deploy.GetInitializers().Pending) > 0 {
-					InitializeDeploy(deploy)
+				configMap := obj.(*core.ConfigMap)
+				fmt.Println("Added configmap:", configMap.Name)
+
+				if configMap.GetInitializers() == nil {
+					fmt.Println("Already initialized configmap:", configMap.Name)
+				} else if configMap.GetInitializers().Pending[0].Name != initializerName {
+					fmt.Println("Not my turn to initialize configmap:", configMap.Name)
+				} else {
+					fmt.Println("Initializing configmap:", configMap.Name)
+					if _, err := initializeConfigmap(kubeClient, configMap); err != nil {
+						fmt.Printf("Failed to initialize configmap %s, reason %s\n", configMap.Name, err)
+					} else {
+						fmt.Println("Initialized configmap:", configMap.Name)
+					}
 				}
 			},
 		},
 	)
 
-	fmt.Println("Running controller")
+	fmt.Println("Running controller...")
 	go controller.Run(make(chan struct{}))
 }
 
-func InitializeDeploy(deploy *apps.Deployment) {
-	fmt.Println("Patching deployment:", deploy.Name)
-	obj, err := PatchDeployment(kubeClient, deploy, func(deploy *apps.Deployment) *apps.Deployment {
-		pending := deploy.Initializers.Pending
-		fmt.Println("Removing pending initializer:", pending[0])
+func initializeConfigmap(kubeClient kubernetes.Interface, configMap *core.ConfigMap) (*core.ConfigMap, error) {
+	return patchConfigMap(kubeClient, configMap, func(configMap *core.ConfigMap) *core.ConfigMap {
+		pending := configMap.Initializers.Pending
+		fmt.Println("Removing pending initializer:", pending[0].Name)
 		if len(pending) == 1 {
-			deploy.ObjectMeta.Initializers = nil
+			configMap.ObjectMeta.Initializers = nil
 		} else {
-			deploy.ObjectMeta.Initializers.Pending = append(pending[:0], pending[1:]...)
+			configMap.ObjectMeta.Initializers.Pending = append(pending[:0], pending[1:]...)
 		}
-		return deploy
+
+		// adding some data to check letter
+		configMap.Data = map[string]string{
+			"initialized-by": initializerName,
+		}
+		return configMap
 	})
-	if err != nil {
-		log.Println(err)
-	}
-	fmt.Println("Patched Initializer:", obj.Initializers)
 }
 
-func InitializerForResources(resources []string) v1alpha1.InitializerConfiguration {
+func patchConfigMap(c kubernetes.Interface, cur *core.ConfigMap, transform func(*core.ConfigMap) *core.ConfigMap) (*core.ConfigMap, error) {
+	curJson, err := json.Marshal(cur)
+	if err != nil {
+		return nil, err
+	}
+	modJson, err := json.Marshal(transform(cur))
+	if err != nil {
+		return nil, err
+	}
+	patch, err := strategicpatch.CreateTwoWayMergePatch(curJson, modJson, apps.Deployment{})
+	if err != nil {
+		return nil, err
+	}
+	if len(patch) == 0 || string(patch) == "{}" {
+		return cur, nil
+	}
+	return c.CoreV1().ConfigMaps(cur.Namespace).Patch(cur.Name, ktypes.StrategicMergePatchType, patch)
+}
+
+func initializerForResources(resources []string) v1alpha1.InitializerConfiguration {
 	return v1alpha1.InitializerConfiguration{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: "my-initializer-config",
@@ -124,7 +164,7 @@ func InitializerForResources(resources []string) v1alpha1.InitializerConfigurati
 		},
 		Initializers: []v1alpha1.Initializer{
 			{
-				Name: "com.my.initializer",
+				Name: initializerName,
 				Rules: []v1alpha1.Rule{
 					{
 						APIGroups:   []string{"*"},
@@ -137,67 +177,5 @@ func InitializerForResources(resources []string) v1alpha1.InitializerConfigurati
 	}
 }
 
-func PodTemplate() core.PodTemplateSpec {
-	return core.PodTemplateSpec{
-		ObjectMeta: metav1.ObjectMeta{
-			Labels: map[string]string{
-				"app": "my-app",
-			},
-		},
-		Spec: core.PodSpec{
-			Containers: []core.Container{
-				{
-					Name:            "busybox",
-					Image:           "busybox",
-					ImagePullPolicy: core.PullIfNotPresent,
-					Command: []string{
-						"sleep",
-						"3600",
-					},
-				},
-			},
-		},
-	}
-}
-
-func Deployment() apps.Deployment {
-	return apps.Deployment{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "my-deploy",
-			Namespace: "default",
-		},
-		Spec: apps.DeploymentSpec{
-			Replicas: func() *int32 { i := int32(1); return &i }(),
-			Template: PodTemplate(),
-		},
-	}
-}
-
-func PatchDeployment(c kubernetes.Interface, cur *apps.Deployment, transform func(*apps.Deployment) *apps.Deployment) (*apps.Deployment, error) {
-	curJson, err := json.Marshal(cur)
-	if err != nil {
-		return nil, err
-	}
-
-	modJson, err := json.Marshal(transform(cur))
-	if err != nil {
-		return nil, err
-	}
-
-	patch, err := strategicpatch.CreateTwoWayMergePatch(curJson, modJson, apps.Deployment{})
-	if err != nil {
-		return nil, err
-	}
-	if len(patch) == 0 || string(patch) == "{}" {
-		return cur, nil
-	}
-	return c.AppsV1beta1().Deployments(cur.Namespace).Patch(cur.Name, ktypes.StrategicMergePatchType, patch)
-}
-
-/*
-minikube start --extra-config=apiserver.Admission.PluginNames="Initializers,NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,ResourceQuota"
-
-kubectl delete initializerconfigurations/my-initializer-config deploy/my-deploy
-
-goimports -w k8s_initializer.go; go run k8s_initializer.go
-*/
+// Enable initializer:
+// minikube start --extra-config=apiserver.Admission.PluginNames="Initializers,NamespaceLifecycle,LimitRanger,ServiceAccount,DefaultStorageClass,ResourceQuota"
